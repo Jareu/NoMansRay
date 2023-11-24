d276c2e805b4a388be2ab0b1c4a3a15e3da22b81
Jamie.Kabzinski <jamie_r_kabzinski@hotmail.com>
2023-11-24 19:05:45 +1030

    implemented Thruster

--- NoMansRay/Thruster.h
+++ NoMansRay/Thruster.h
@@ -1,18 +1,22 @@
 #pragma once
 #include "Actor.h"
 
-class Thruster : Actor
+class Thruster : public Actor
 {
 private:
-	float throttle_;
+	decimal throttle_;
+	Vector2<decimal> thrust_;
 
+	void update_thrust();
 public:
 	const decimal MAX_THRUST;
 
 	Thruster() = delete;
-	Thruster(Universe& universe, float max_thrust);
+	Thruster(Universe& universe, decimal max_thrust);
 	~Thruster() = default;
 
-	void setThrottle(float throttle);
-	float getThrottle();
+	void setThrottle(decimal throttle);
+	decimal getThrottle() const;
+	Vector2<decimal> getThrust() const;
+	virtual void setRotation(const decimal& new_rotation) override;
 };
